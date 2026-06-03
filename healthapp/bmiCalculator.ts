fa127d8b63b7dc530ad538a46b7910072ef17f1c
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2026-06-03 15:58:56 +0300

    Tehtävä 9.5: Lisätty BMI‑endpoint Express‑palvelimeen

--- healthapp/bmiCalculator.ts
+++ healthapp/bmiCalculator.ts
@@ -24,13 +24,15 @@ export const calculateBmi = (height: number, weight: number): string => {
   return "Obese";
 };
 
-try {
-  const { height, weight } = parseBmiArguments(process.argv);
-  console.log(calculateBmi(height, weight));
-} catch (error: unknown) {
-  let errorMessage = "Something went wrong.";
-  if (error instanceof Error) {
-    errorMessage += " Error: " + error.message;
+if (process.argv[1] === import.meta.filename) {
+  try {
+    const { height, weight } = parseBmiArguments(process.argv);
+    console.log(calculateBmi(height, weight));
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong.";
+    if (error instanceof Error) {
+      errorMessage += " Error: " + error.message;
+    }
+    console.log(errorMessage);
   }
-  console.log(errorMessage);
 }
--- healthapp/index.ts
+++ healthapp/index.ts
@@ -1,9 +1,33 @@
 import express from "express";
+import { calculateBmi } from "./bmiCalculator.ts";
 
 const app = express();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  const { height, weight } = req.query;
+
+  if (!height || !weight) {
+    return res.status(400).json({ error: "malformatted parameters" });
+  }
+
+  const h = Number(height);
+  const w = Number(weight);
+
+  if (isNaN(h) || isNaN(w)) {
+    return res.status(400).json({ error: "malformatted parameters" });
+  }
+
+  const bmi = calculateBmi(h, w);
+
+  return res.json({
+    height: h,
+    weight: w,
+    bmi,
+  });
 });
 
 const PORT = 3003;
