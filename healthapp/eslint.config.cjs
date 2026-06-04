b3b83131188bf4064dbac74f8dd9f38bf7ef3df3
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2026-06-04 14:05:54 +0300

    Tehtävä 9.6: Lisätty ESLint-konfiguraatio ja korjattu varoitukset

---
+++ healthapp/eslint.config.cjs
@@ -0,0 +1,27 @@
+const js = require("@eslint/js");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+const tsParser = require("@typescript-eslint/parser");
+
+module.exports = [
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        console: "readonly",
+        process: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+];
