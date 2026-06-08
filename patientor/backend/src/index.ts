04b76341e6db855bd8fd2a8aa9b7c5c1459d09ed
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2026-06-08 17:28:40 +0300

    Tehtävä 9.9: Alustettu backend, lisätty ESLint ja /api/ping‑endpoint

---
+++ patientor/backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = 3001;
+
+app.get("/api/ping", (_req, res) => {
+  res.send("pong");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
