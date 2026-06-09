34d60f89b0eebe908f769f0b143fb55808496e29
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2026-06-09 13:48:18 +0300

    Tehtävä 9.10: Frontend lisätty ja ping‑endpoint korjattu (CORS), backend käynnistetty + disabloitu workflowt

--- patientor/backend/src/index.ts
+++ patientor/backend/src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 3001;
+
+app.use(cors());
 
 app.get("/api/ping", (_req, res) => {
   res.send("pong");
